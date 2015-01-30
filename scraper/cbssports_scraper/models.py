a0813a93f5a06456febdcb4a38d156fa714b1c31
Matthew Todd Hayton <mhayton@Todds-MBP.home>
2015-01-30 11:54:27 -0500

    new fields and defaults

--- scraper/cbssports_scraper/models.py
+++ scraper/cbssports_scraper/models.py
@@ -1,3 +1,4 @@
+import datetime
 from django.db import models
 
 # Create your models here.
@@ -5,11 +6,18 @@ class Position(models.Model):
     name = models.CharField(max_length=64)
     url = models.URLField(max_length=256)
 
+    def __str__(self):
+        return self.name
+
 class Player(models.Model):
     position = models.ForeignKey(Position)
     name = models.CharField(max_length=64)
+    rank = models.IntegerField(default=0)
     url = models.URLField(max_length=256)
     team_name = models.CharField(max_length=64)
     team_url  = models.URLField(max_length=256)
     team_code = models.CharField(max_length=3)
-    birthdate = models.DateField()
+    birthdate = models.DateField(default=datetime.date.today)
+
+    def __str__(self):
+        return self.name
